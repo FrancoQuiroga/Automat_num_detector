3e563ea922cd3454a80d1f000e5d3159b8e29496
FrancoQuiroga <fe.quiroga@um.edu.ar>
2024-04-01 13:49:18 -0300

    Agrega una funcion que es un automata

--- automatTP1.py
+++ automatTP1.py
@@ -1,7 +1,6 @@
 import unittest
 def recon_num(cadena):
     cadena = str(cadena)
-    enteros = '1234567890'
     diccionarioestados = {
         '0':
         { '-':1, '+':1, '1':2, '2':2, '3':2, '4':2, '5':2, '6':2, '7':2, '8':2, '9':2, '0':2},
